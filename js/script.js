ee1650beb2daa1d61121b721c4110481c6d0a31e
farhan135027 <164714584+farhan135027@users.noreply.github.com>
2026-06-25 21:50:33 +0800

    Initialized Git for project.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function openSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    sidebar.style.visibility = "visible";
+}
+
+function closeSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    sidebar.style.visibility = "hidden";
+}
+
+function openLogin() {
+    var login = document.getElementById("login");
+    login.style.visibility = "visible";
+}
+
+function closeLogin() {
+    var login = document.getElementById("login");
+    login.style.visibility = "hidden";
+}
+
+function openRegister() {
+    var register = document.getElementById("register");
+    register.style.visibility = "visible";
+}
+
+function closeRegister() {
+    var register = document.getElementById("register");
+    register.style.visibility = "hidden";
+}
